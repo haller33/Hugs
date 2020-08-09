d46377a5e0d0b72f978c6e33369e22fde1da4948
haller218 <eliseu42lucena@gmail.com>
2020-08-09 14:36:52 +0000

    add files

---
+++ src/platform.h
@@ -0,0 +1,34 @@
+/*
+ * src/platform.h.  Generated from platform.h.in by configure.
+ * configure-sussed platform #defines.
+ */
+#ifndef __PLATFORM_H__
+#define __PLATFORM_H__
+
+#define HostPlatform   armv7l_unknown_linux
+#define TargetPlatform armv7l_unknown_linux
+#define BuildPlatform  armv7l_unknown_linux
+
+/* Definitions suitable for use in CPP conditionals */
+#define armv7l_unknown_linux_HOST 1
+#define armv7l_unknown_linux_TARGET 1
+#define armv7l_unknown_linux_BUILD 1
+
+#define armv7l_HOST_ARCH 1
+#define armv7l_TARGET_ARCH 1
+#define armv7l_BUILD_ARCH 1
+
+#define linux_HOST_OS 1
+#define linux_TARGET_OS 1
+#define linux_BUILD_OS 1
+
+/* Definitions of strings for use in C or Haskell code */
+#define HOST_ARCH	"armv7l"
+#define TARGET_ARCH	"armv7l"
+#define BUILD_ARCH	"armv7l"
+
+#define HOST_OS		"linux"
+#define TARGET_OS	"linux"
+#define BUILD_OS	"linux"
+
+#endif /* __PLATFORM_H__ */
